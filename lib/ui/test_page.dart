793c1bb81cc178d9f5a793d44a69fb1dba3f0efe
ananda606 <85835149+ananda606@users.noreply.github.com>
2022-11-06 11:28:17 +0700

    product list update

--- lib/ui/test_page.dart
+++ lib/ui/test_page.dart
@@ -51,59 +51,68 @@ class _testState extends State<test> {
                       );*/
                     })),*/
             ),
-            Container(
-              height: MediaQuery.of(context).size.height,
-              width: 200,
-              child: GridView.builder(
-                primary: false,
-                padding: const EdgeInsets.all(20),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.red,
                 ),
-                itemBuilder: (context, index) {
-                  return RawMaterialButton(
-                    onPressed: () {},
-                    child: Hero(
-                      tag: 'logo$index',
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
+                child: GridView.builder(
+                  primary: false,
+                  padding: const EdgeInsets.all(20),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) {
+                    return ElevatedButton(
+                      onPressed: () {},
+                      child: Hero(
+                        tag: 'logo$index',
+                        child: Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Center(child: Text("abc ${index + 1}")),
                         ),
-                        child: Text("abc ${index + 1}"),
                       ),
-                    ),
-                  );
-                },
-                itemCount: 10,
+                    );
+                  },
+                  itemCount: 10,
 
-                /*
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          color: Colors.teal[100],
-                          child: const Text("He'd have you all unravel at the"),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          color: Colors.teal[200],
-                          child: const Text('Heed not the rabble'),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          color: Colors.teal[300],
-                          child: const Text('Sound of screams but the'),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          color: Colors.teal[400],
-                          child: const Text('Who scream'),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          color: Colors.teal[500],
-                          child: const Text('Revolution is coming...'),
-                        ),*/
+                  /*
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            color: Colors.teal[100],
+                            child: const Text("He'd have you all unravel at the"),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            color: Colors.teal[200],
+                            child: const Text('Heed not the rabble'),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            color: Colors.teal[300],
+                            child: const Text('Sound of screams but the'),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            color: Colors.teal[400],
+                            child: const Text('Who scream'),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            color: Colors.teal[500],
+                            child: const Text('Revolution is coming...'),
+                          ),*/
+                ),
               ),
             ),
           ],
